33cc5c114b37a2e6861378f740ffcf9cc5ad7eee
Arild Andreassen <arildsa@hotmail.com>
2022-07-22 16:59:06 -0600

    cleaning up tests

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,4 +6,79 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+const list = (req, res, next) => {
+  res.json({ data: dishes });
+};
+
+const read = (req, res, next) => {
+  const { dish } = res.locals;
+  res.json({ data: dish });
+};
+
+const create = (req, res, next) => {
+  const { dish } = res.locals;
+  const newDish = {
+    ...dish,
+    id: nextId(),
+  };
+  dishes.push(newDish);
+  res.status(200).json({ data: newDish });
+};
+
+const update = (req, res, next) => {
+  const { dish } = res.locals;
+  res.status(200).json({ data: dish });
+};
+
+const validateExists = (req, res, next) => {
+  const { dishId } = req.params;
+  const dish = dishes.find((dish) => dish.id === dishId);
+
+  if (dish) {
+    res.locals.dish = dish;
+    res.locals.dishId = dishId;
+    return next();
+  }
+
+  return next({ status: 404, message: `Dish does not exist: ${dishId}` });
+};
+
+const validateBody = (req, res, next) => {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  if (!name || name === "") {
+    return next({ status: 400, message: "Dish must include a name" });
+  } else if (!description || description === "") {
+    return next({ status: 400, message: "Dish must include a description" });
+  } else if (!price) {
+    return next({ status: 400, message: "Dish must include a price" });
+  } else if (typeof price !== "number" || price < 0) {
+    return next({
+      status: 400,
+      message: "Dish must have a price that is an integer grater than 0",
+    });
+  } else if (!image_url || image_url === "") {
+    return next({ status: 400, message: "Dish must include a image_url" });
+  } else {
+    res.locals.dish = req.body.data;
+    return next();
+  }
+};
+
+const validateMatch = (req, res, next) => {
+  const { dishId, dish } = res.locals;
+  if (dishId !== dish.id) {
+    return next({
+      status: 404,
+      message: `Dish id does not match route id. Dish: ${dish.id}, Route: ${dishId}`,
+    });
+  }
+
+  return next();
+};
+
+module.exports = {
+  list,
+  read: [validateExists, read],
+  create: [validateBody, create],
+  update: [validateExists, validateBody, validateMatch, update],
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,143 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+const validDeliveryStatus = [
+  "pending",
+  "preparing",
+  "out-for-delivery",
+  "delivered",
+];
+
+const list = (req, res, next) => {
+  res.json({ data: orders });
+};
+
+const create = (req, res, next) => {
+  const { order } = res.locals;
+  const newOrder = {
+    ...order,
+    id: nextId(),
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+};
+
+const read = (req, res, next) => {
+  const { order } = res.locals;
+  res.json({ data: order });
+};
+
+const update = (req, res, next) => {
+  const { order, index } = res.locals;
+
+  orders.splice(index, 1);
+  orders.push(order);
+  res.status(201).json({ data: order });
+};
+
+const destroy = (req, res, next) => {
+  const { index, order } = res.locals;
+  orders.splice(index, 1);
+  res.status(204).json({ data: order });
+};
+
+const validateExists = (req, res, next) => {
+  const { orderId } = req.params;
+  const order = orders.find((order) => order.id === orderId);
+  const index = orders.findIndex((order) => order.id === orderId);
+
+  if (order) {
+    res.locals.order = order;
+    res.locals.index = index;
+    res.locals.orderId = orderId;
+    return next();
+  }
+
+  return next({ status: 404, message: `Order ${orderId} not found` });
+};
+
+const validateBody = (req, res, next) => {
+  const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
+  if (!deliverTo || deliverTo === "") {
+    return next({
+      status: 400,
+      message: "Order must include a deliverTo",
+    });
+  } else if (!mobileNumber || mobileNumber === "") {
+    return next({
+      status: 400,
+      message: "Order must include a mobileNumber",
+    });
+  } else if (!dishes) {
+    return next({
+      status: 400,
+      message: "Order must include a dish",
+    });
+  } else if (dishes === []) {
+    return next({
+      status: 400,
+      message: "Order must include at least one dish",
+    });
+  }
+
+  for (const [index, dish] of dishes.entries()) {
+    if (
+      dish.quantity === undefined ||
+      typeof dish.quantity !== "number" ||
+      dish.quantity < 0
+    ) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  }
+
+  res.locals.order = req.body.data;
+  return next();
+};
+
+const validateUpdate = (req, res, next) => {
+  const { data: { id, status } = {} } = req.body;
+  const { orderId } = req.params;
+  const { order } = res.locals;
+  if (id !== orderId) {
+    return next({
+      status: 404,
+      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}.`,
+    });
+  } else if (!validDeliveryStatus.includes(status)) {
+    return next({
+      status: 400,
+      message:
+        "Order must have a status of pending, preparing, out-for-delivery, delivered",
+    });
+  } else if (order.status === "delivered") {
+    return next({
+      status: 400,
+      message: "A delivered order cannot be changed",
+    });
+  }
+
+  next();
+};
+
+const canBeDestroyed = (req, res, next) => {
+  const { order } = res.locals;
+  if (order.status !== "pending") {
+    return next({
+      status: 400,
+      message: "An order cannot be deleted unless it is pending",
+    });
+  }
+
+  next();
+};
+
+module.exports = {
+  list,
+  read: [validateExists, read],
+  create: [validateBody, create],
+  update: [validateExists, validateBody, validateUpdate, update],
+  destroy: [validateExists, canBeDestroyed, destroy],
+};
